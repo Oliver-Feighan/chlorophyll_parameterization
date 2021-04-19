75c6208eaf8a2405cdc7c33ae02ee77b1d4aa2d3
oliver-feighan <oliverfeighan1996@gmail.com>
2021-04-19 09:17:19 +0100

    run sto dscf

--- tddft_data/write_files.sh
+++ tddft_data/write_files.sh
@@ -2,7 +2,9 @@ for i in */;
 	do 
 	new_file=${i/\//}
 	#echo ${i}dscf_${new_file}.in
-	sed "s/NAME/${new_file}/g" dscf_template.sub > ${i}dscf_${new_file}.sub
-	sed "s/NAME/${new_file}/g" dscf_template.in > ${i}dscf_${new_file}.in
+	#sed "s/NAME/${new_file}/g" dscf_template.sub > ${i}dscf_${new_file}.sub
+	#sed "s/NAME/${new_file}/g" dscf_template.in > ${i}dscf_${new_file}.in
+	sed "s/NAME/${new_file}/g" sto_template.sub > ${i}sto_${new_file}.sub
+	sed "s/NAME/${new_file}/g" sto_template.in > ${i}sto_${new_file}.in
 done
 
