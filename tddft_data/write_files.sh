639083e62aed0f6485dc713c625ae241868ec27c
oliver-feighan <oliverfeighan1996@gmail.com>
2021-05-04 09:13:54 +0100

    run camb3lyp tddft

--- tddft_data/write_files.sh
+++ tddft_data/write_files.sh
@@ -7,8 +7,8 @@ for i in */;
 	#sed "s/NAME/${new_file}/g" sto_template.sub > ${i}sto_${new_file}.sub
 	#sed "s/NAME/${new_file}/g" sto_template.in > ${i}sto_${new_file}.in
 	
-	#sed "s/NAME/${new_file}/g" camb3lyp_template.sub > ${i}camb3lyp_${new_file}.sub
-	#sed "s/NAME/${new_file}/g" camb3lyp_template.in > ${i}camb3lyp_${new_file}.in
+	sed "s/NAME/${new_file}/g" camb3lyp_template.sub > ${i}camb3lyp_${new_file}.sub
+	sed "s/NAME/${new_file}/g" camb3lyp_template.in > ${i}camb3lyp_${new_file}.in
 
 	#sed "s/NAME/${new_file}/g" wB97X_template.sub > ${i}wB97X_${new_file}.sub
 	#sed "s/NAME/${new_file}/g" wB97X_template.in > ${i}wB97X_${new_file}.in
@@ -16,7 +16,7 @@ for i in */;
 	#sed "s/NAME/${new_file}/g" BLYP_template.sub > ${i}BLYP_${new_file}.sub
 	#sed "s/NAME/${new_file}/g" BLYP_template.in > ${i}BLYP_${new_file}.in
 	
-	sed "s/NAME/${new_file}/g" eigdiff_template.sub > ${i}eigdiff_${new_file}.sub
-	sed "s/NAME/${new_file}/g" eigdiff_template.in > ${i}eigdiff_${new_file}.in
+	#sed "s/NAME/${new_file}/g" eigdiff_template.sub > ${i}eigdiff_${new_file}.sub
+	#sed "s/NAME/${new_file}/g" eigdiff_template.in > ${i}eigdiff_${new_file}.in
 done
 
