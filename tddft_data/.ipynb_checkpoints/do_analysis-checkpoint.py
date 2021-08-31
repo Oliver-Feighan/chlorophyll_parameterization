f1b6ca6d2020ae7cae986e3fdc5c9213e85fcee4
Oliver-Feighan <oliverfeighan1996@gmail.com>
2021-08-31 10:55:28 +0100

    analysis

---
+++ tddft_data/.ipynb_checkpoints/do_analysis-checkpoint.py
@@ -0,0 +1,123 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import re
+import json
+import os
+
+##Rip transition properties from output files. If transition properties don't exist, 
+##	add the xyz file to the re-run pile.
+molecules = os.listdir(path='.')
+
+#coordinate lines for the N_A, N_C atoms 
+Na_line = 18
+Nc_line = 35
+
+output_files = []
+xyz_files = []
+
+for i in molecules:
+	if i.endswith(".out"):
+		output_files.append(i)
+
+	elif i.endswith(".xyz"):
+		xyz_files.append(i)
+
+results = {}
+
+rerun_pile = []
+
+no_homo_lumo_trans = []
+
+for file in output_files:
+	# last number should be the transition dipole line
+	excited_states = []
+
+	lines = list(open(file, 'r'))
+	for num, line in enumerate(lines):
+		if "Excited State" in line or "Transition Dipole Moments" in line:
+			excited_states.append(num)
+
+	if not excited_states:
+		rerun_pile.append(file)
+		continue
+
+	transition_dipole_start = excited_states[-1]
+
+	homo_lumo_coeff = 0
+	homo_lumo_energy = 0
+	homo_lumo_trans = 0
+
+	for state in range(len(excited_states)-1):
+		state_lines = lines[excited_states[state]:excited_states[state+1]]
+		for line in state_lines:
+			if "->" in line:
+				mo_coeff = re.findall(r'-?\d+\.?\d+', line)
+				occ, virt, coeff = mo_coeff
+				if int(virt) - int(occ) == 1:
+					if abs(float(coeff)) > homo_lumo_coeff:
+						homo_lumo_coeff = float(coeff)
+						homo_lumo_trans = state
+
+						energy_hr, energy_eV, S_2, Multiplicity = re.findall(r'-?\d+\.\d+', state_lines[0])
+
+						homo_lumo_energy = float(energy_hr)
+
+	transition_dipole_line = transition_dipole_start + 2 + homo_lumo_trans
+	x, y, z = re.findall(r'-?\d*\.\d*', lines[transition_dipole_line])
+
+	assert(homo_lumo_energy != 0)
+
+	xyz_file = file.replace('.out', '.xyz')
+	
+	coords = list(open(xyz_file))
+
+	Na = np.array([float(x) for x in re.findall(r'-?\d+\.\d+', coords[Na_line])])
+	Nc = np.array([float(x) for x in re.findall(r'-?\d+\.\d+', coords[Nc_line])])
+
+	Na_Nc = Na-Nc
+
+	results[file.replace(".out", "")] = {
+			"energy" : homo_lumo_energy, 
+			"transition_dipole" : [float(x), float(y), float(z)],
+			"Na_Nc" : Na_Nc.tolist()
+	}
+
+## Analysis
+energies = []
+Na_Nc_lengths = []
+transition_dipole_mags = []
+angle_errors = []
+
+def angle_error(vec1, vec2):
+	numerator = np.dot(vec1, vec2)
+	denominator = np.linalg.norm(vec1) * np.linalg.norm(vec2)
+
+	return np.rad2deg(np.arccos(numerator/denominator))
+
+def angle_error_with_phase(vec1, vec2):
+	return min(angle_error(vec1, vec2), angle_error(-vec1, vec2))
+
+for chrom in results.values():
+	Na_Nc_lengths.append(np.linalg.norm(np.array(chrom["Na_Nc"])))
+	transition_dipole_mags.append(np.linalg.norm(chrom["transition_dipole"]))
+	energies.append(chrom["energy"])
+	angle_errors.append(angle_error_with_phase(np.array(chrom["transition_dipole"]), np.array(chrom["Na_Nc"])))
+
+def plot_histogram(values, title):
+	fig, ax = plt.subplots()
+	ax.set_title(title)
+	ax.hist(values, bins=30)
+
+plot_histogram(np.array(energies), "energies")
+plot_histogram(np.array(Na_Nc_lengths), "$N_A$-$N_C$ lengths")
+plot_histogram(np.array(transition_dipole_mags), "transition dipone magnitudes")
+plot_histogram(np.array(angle_errors), "angle error to $Q_y$ axis")
+
+plt.show()
+
+#rerun_pile_file = open('rerun_pile.txt', 'w')
+#for i in rerun_pile:
+#	print(i, file=rerun_pile_file)
+
+#with open('tddft_data.json', 'w') as dump_file:
+#	json.dump(results, sort_keys=True, indent=4, fp=dump_file)
---
+++ tddft_data/reference_methods.py
@@ -0,0 +1,173 @@
+import os
+import re
+import json
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from scipy.stats import linregress
+
+def angle_error(vec1, vec2):
+    numerator = np.dot(vec1, vec2)
+    denominator = np.linalg.norm(vec1) * np.linalg.norm(vec2)
+
+    return np.rad2deg(np.arccos(numerator/denominator))
+
+def angle_error_with_phase(vec1, vec2):
+    return min(angle_error(vec1, vec2), angle_error(-vec1, vec2))
+
+def get_Na_Nc_vec(xyz_file):
+    coords = list(open(xyz_file))
+    
+    Na_line = 18
+    Nc_line = 35
+
+    Na = np.array([float(x) for x in re.findall(r'-?\d+\.\d+', coords[Na_line])])
+    Nc = np.array([float(x) for x in re.findall(r'-?\d+\.\d+', coords[Nc_line])])
+
+    return(Na-Nc)
+
+def read_td_result(chromophore_name, method, root):
+
+    file_name = f"{root}/{chromophore_name}/{method}_{chromophore_name}.out"
+    
+    if method == "PBE0":
+        file_name = f"{root}/{chromophore_name}/{chromophore_name}.out"
+        
+    last_transition_energy = 0.
+    last_index = None
+
+    homo_lumo_energy = 0.
+    homo_lumo_coeff = 0.
+    homo_lumo_index = None
+
+    transition_dipole = None
+
+    lines = list(open(file_name))
+    for enum, line in enumerate(lines):
+        if "Excited State" in line:
+            last_index = int(re.findall(r'\d+', line)[0])
+            last_transition_energy = float(re.findall(r'\d+.\d+', line)[0])
+
+        if "->" in line:
+            orbitals = [int(x) for x in re.findall(r'\d+', line)[:2]]
+            coeff = abs(float(re.findall(r'-?\d+.\d+', line)[2]))
+
+            if orbitals[1] - orbitals[0] == 1 and coeff > homo_lumo_coeff:
+                homo_lumo_energy = last_transition_energy
+                homo_lumo_index = last_index
+                homo_lumo_coeff = coeff
+        
+        if "Transition Dipole Moments (a.u.)" in line:
+            transition_dipole = np.array([float(x) for x in re.findall(r'-?\d+.\d+', lines[enum+1+homo_lumo_index])])
+
+    Na_Nc_vec = get_Na_Nc_vec(f"{root}/{chromophore_name}/{chromophore_name}.xyz")
+    angle_error = angle_error_with_phase(Na_Nc_vec, transition_dipole)
+
+    return homo_lumo_energy, transition_dipole, angle_error
+
+def read_td_method(method, root="."):    
+    results = {
+        "chromophore" : [],
+        f"{method} excitation energy (hr)" : [],
+        f"{method} excitation energy (eV)" : [],
+        f"{method} transition dipole" : [],
+        f"{method} transition dipole magnitude" : [],
+        f"{method} angle to Na_Nc" : []
+    }
+    
+    steps = range(1, 1000, 50)
+    chromophores = range(1, 28, 1)
+    
+    for step in steps:
+        for chromophore in chromophores:
+            chromophore_name = f"step_{step}_chromophore_{chromophore}"
+
+            try:
+                homo_lumo_energy, transition_dipole, angle_error = read_td_result(chromophore_name, method, root)
+        
+                results["chromophore"].append(chromophore_name)
+                results[f"{method} excitation energy (hr)"].append(homo_lumo_energy)
+                results[f"{method} excitation energy (eV)"].append(homo_lumo_energy * 27.2114)
+                results[f"{method} transition dipole"].append(transition_dipole)
+                results[f"{method} transition dipole magnitude"].append(np.linalg.norm(transition_dipole))
+                results[f"{method} angle to Na_Nc"].append(angle_error)
+            except:
+                continue
+            
+    df_result = pd.DataFrame.from_dict(results)
+    return df_result.set_index("chromophore")
+
+
+def read_excited_scf_result(chromophore_name, method, root):
+    file_name = f"{root}/{chromophore_name}/{method}_{chromophore_name}.out"
+
+    lines = list(open(f'{file_name}'))
+            
+    energy = None
+    transition_dipole = None
+            
+    for line in lines:
+        if "Excitation energy:" in line:
+            energy_hr = float(re.findall(r'\d+\.\d+', line)[0])
+                    
+        if "Transition dipole" in line:
+            transition_dipole = np.array([float(x) for x in re.findall(r'-?\d+\.\d+', line)])
+
+    Na_Nc_vec = get_Na_Nc_vec(f'{root}/{chromophore_name}/{chromophore_name}.xyz')
+    magnitude = np.linalg.norm(transition_dipole)
+    angle_error = angle_error_with_phase(Na_Nc_vec, transition_dipole)
+
+    return energy, transition_dipole, angle_error 
+    
+def read_excited_scf_method(method, root="."):    
+    results = {
+        "chromophore" : [],
+        f"{method} excitation energy (hr)" : [],
+        f"{method} excitation energy (eV)" : [],
+        f"{method} transition dipole" : [],
+        f"{method} transition dipole magnitude" : [],
+        f"{method} angle to Na_Nc" : []
+    }
+    
+    steps = range(1, 1000, 50)
+    chromophores = range(1, 28, 1)
+    
+    for step in steps:
+        for chromophore in chromophores:
+            chromophore_name = f"step_{step}_chromophore_{chromophore}"
+
+            try:
+                energy, transition_dipole, angle_error = read_excited_scf_result(chromophore_name, method, root)
+            
+                if energy == None or transition_dipole == None:
+                    raise
+            
+                results["chromophore"].append(chromophore)
+                results[f"{method} excitation energy (hr)"].append(energy)
+                results[f"{method} excitation energy (eV)"].append(energy * 27.2114)
+                results[f"{method} transition dipole"].append(transition_dipole)
+                results[f"{method} transition dipole magnitude"].append(np.linalg.norm(transition_dipole))
+                results[f"{method} angle to Na_Nc"].append(angle_error)
+
+            except:
+                continue
+                
+            
+    df_result = pd.DataFrame.from_dict(results)
+    return df_result.set_index("chromophore")
+
+def merge_multiple(dfs, so_far=None):
+    if so_far is None:
+        so_far = pd.merge(dfs[-1], dfs[-2], left_index=True, right_index=True)
+        return merge_multiple(dfs[:-2], so_far)
+    
+    if len(dfs) == 1:
+        so_far = pd.merge(dfs[0], so_far, left_index=True, right_index=True)
+        return so_far
+    
+    if len(dfs) == 0:
+        return so_far        
+    
+    if len(dfs) > 1:
+        so_far = pd.merge(dfs[-1], so_far, left_index=True, right_index=True)
+        return merge_multiple(dfs[:-1], so_far)
