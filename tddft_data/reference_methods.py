c38142f60a28b0fd5b3f95399de2019a0be719ae
Oliver-Feighan <oliverfeighan1996@gmail.com>
2022-05-06 12:27:42 +0100

    update notebook analysis

--- tddft_data/reference_methods.py
+++ tddft_data/reference_methods.py
@@ -27,6 +27,77 @@ def get_Na_Nc_vec(xyz_file):
 
     return Na-Nc
 
+def read_gaussian_result(chromophore_name, method, root):
+
+    file_name = f"{root}/{chromophore_name}/{method}_{chromophore_name}.log"
+        
+    last_transition_energy = 0.
+    last_index = None
+
+    homo_lumo_energy = 0.
+    homo_lumo_coeff = 0.
+    homo_lumo_index = None
+
+    transition_dipoles = []
+
+    lines = list(open(file_name))
+    for enum, line in enumerate(lines):
+        if "Ground to excited state transition electric dipole moments (Au):" in line:
+            for i in range(5):
+                transition_dipole = np.array([float(x) for x in re.findall(r'-?\d+.\d+', lines[enum+2+i])[:3]])
+                
+                transition_dipoles.append(transition_dipole)
+        
+        
+        if "Excited State" in line:
+            last_index = int(re.findall(r'\d+', line)[0])
+            last_transition_energy = float(re.findall(r'\d+.\d+', line)[0])
+
+        if "->" in line:
+            orbitals = [int(x) for x in re.findall(r'\d+', line)[:2]]
+            coeff = abs(float(re.findall(r'-?\d+.\d+', line)[2]))
+
+            if orbitals[1] - orbitals[0] == 1 and coeff > homo_lumo_coeff:
+                homo_lumo_energy = last_transition_energy
+                homo_lumo_index = last_index
+                homo_lumo_coeff = coeff
+               
+    Qy_dipole = transition_dipoles[homo_lumo_index-1]
+
+    Na_Nc_vec = get_Na_Nc_vec(f"{root}/{chromophore_name}/{chromophore_name}.xyz")
+    angle_error = angle_error_with_phase(Na_Nc_vec, Qy_dipole)
+
+    return homo_lumo_energy, Qy_dipole, angle_error
+
+def read_gaussian_method(method, root="."):    
+    results = {
+        "chromophore" : [],
+        f"{method} excitation energy (hr)" : [],
+        f"{method} excitation energy (eV)" : [],
+        f"{method} transition dipole" : [],
+        f"{method} transition dipole magnitude" : [],
+        f"{method} angle to Na_Nc" : []
+    }
+    
+    steps = range(1, 1900, 50)
+    chromophores = range(1, 28, 1)
+    
+    for step in steps:
+        for chromophore in chromophores:
+            chromophore_name = f"step_{step}_chromophore_{chromophore}"
+
+            homo_lumo_energy, transition_dipole, angle_error = read_gaussian_result(chromophore_name, method, root)
+
+            results["chromophore"].append(chromophore_name)
+            results[f"{method} excitation energy (hr)"].append(homo_lumo_energy / 27.2114)
+            results[f"{method} excitation energy (eV)"].append(homo_lumo_energy)
+            results[f"{method} transition dipole"].append(transition_dipole)
+            results[f"{method} transition dipole magnitude"].append(np.linalg.norm(transition_dipole))
+            results[f"{method} angle to Na_Nc"].append(angle_error)
+            
+    df_result = pd.DataFrame.from_dict(results)
+    return df_result.set_index("chromophore")
+
 def read_td_result(chromophore_name, method, root):
 
     file_name = f"{root}/{chromophore_name}/{method}_{chromophore_name}.out"
@@ -76,7 +147,7 @@ def read_td_method(method, root="."):
         f"{method} angle to Na_Nc" : []
     }
     
-    steps = range(1, 1000, 50)
+    steps = range(1, 1900, 50)
     chromophores = range(1, 28, 1)
     
     for step in steps:
@@ -132,7 +203,7 @@ def read_excited_scf_method(method, root="."):
         f"{method} angle to Na_Nc" : []
     }
     
-    steps = range(1, 1000, 50)
+    steps = range(1, 1900, 50)
     chromophores = range(1, 28, 1)
     
     for step in steps:
