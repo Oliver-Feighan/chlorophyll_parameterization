5be34dc683e4404e75b9955b57e64bf9ad1cd85f
Oliver-Feighan <oliverfeighan1996@gmail.com>
2022-05-06 10:03:32 +0100

    finish DFT functionals run

--- tddft_data/write_gaussian_files.sh
+++ tddft_data/write_gaussian_files.sh
@@ -12,30 +12,26 @@ for i in step*/;
         #write TDDFT files
         for i in ${!functionals[@]};
             do    
-                sed -e "s/FUNC/${functionals[$i]}/g" -e "s/NAME/${bchla_name}/g" gaussian_tddft_template.txt;
-                sed "1d;2d" ${xyz_file}
-                echo
+                sed -e "s/FUNC/${functionals[$i]}/g" -e "s/NAME/${bchla_name}/g" gaussian_tddft_template.txt > ${bchla_name}/${methods[$i]}_${bchla_name}.com;
+                sed "1d;2d" ${xyz_file} >> ${bchla_name}/${methods[$i]}_${bchla_name}.com;
+                echo >> ${bchla_name}/${methods[$i]}_${bchla_name}.com;
         done;
 
         #write ZINDO file
-        sed "1d;2d" ${xyz_file}
-        echo
+	sed "s/NAME/${bchla_name}/g" gaussian_zindo_template.txt > ${bchla_name}/ZINDO_${bchla_name}.com
+        sed "1d;2d" ${xyz_file} >> ${bchla_name}/ZINDO_${bchla_name}.com 
+        echo >> ${bchla_name}/ZINDO_${bchla_name}.com
         
         #write submission script
         g16_string="time g16 < COM > LOG"
         
-        sed "s/NAME/${bchla_name}/g" gaussian_submission_template.txt
+        sed "s/NAME/${bchla_name}/g" gaussian_submission_template.txt > ${bchla_name}/${bchla_name}_gaussian.sub
     
         for i in ${!methods[@]};
             do
-                method_string=$(echo ${g16_string} | sed -e "s/COM/${methods[$i]}_${bchla_name}.com/g" -e "s/LOG/${methods[$i]}_${bchla_name}.com/g");
-                
-                sed "s/${methods[${i}]}/${method_string}/g" gaussian_submission_template.txt
+                method_string=$(echo ${g16_string} | sed -e "s/COM/${methods[$i]}_${bchla_name}.com/g" -e "s/LOG/${methods[$i]}_${bchla_name}.log/g");
+                sed -i "s/${methods[${i}]}/${method_string}/g" ${bchla_name}/${bchla_name}_gaussian.sub
+		
         done
-
-    exit 0
-        
 done
-
-
-        
+exit 0
